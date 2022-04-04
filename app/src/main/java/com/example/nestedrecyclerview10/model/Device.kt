5589d04fefb0a5fd7b096c82351f73ab7e16cf08
hhy09020 <hhy09020@vinetech.com>
2022-04-04 17:52:08 +0900

    i

---
+++ app/src/main/java/com/example/nestedrecyclerview10/model/Device.kt
@@ -0,0 +1,3 @@
+package com.example.nestedrecyclerview10.model
+
+data class Device(val name : String)
---
+++ app/src/main/java/com/example/nestedrecyclerview10/model/Room.kt
@@ -0,0 +1,3 @@
+package com.example.nestedrecyclerview10.model
+
+data class Room(val name : String)
